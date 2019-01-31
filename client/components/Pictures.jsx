c93c66dcc926c8c621cbb4c175cdf0a30a3e16c6
Nat Grant <email.natgrant@gmail.com>
2019-02-01 09:36:19 +1300

    react environ setup

---
+++ client/components/Pictures.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pictures = props => {
+  //Need the three images to be added here with click events
+};
+
+export default Pictures;
---
+++ data/pictures.js
@@ -0,0 +1,17 @@
+const people = [
+  {
+    id: 1,
+    name: Bob,
+    photo: "bob.jpg"
+  },
+  {
+    id: 2,
+    name: Bob,
+    photo: "bob.jpg"
+  },
+  {
+    id: 3,
+    name: Bob,
+    photo: "bob.jpg"
+  }
+];
